81455e5630b524d92e0337a1cb8ae6a30664a4b4
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-08-15 12:23:31 +0300

    update the styling in the app

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,11 +1,25 @@
-import img from "../../images/male.svg";
+import img1 from "../../images/home (1).jpg";
+import img2 from "../../images/home (2).jpg";
+import img3 from "../../images/home (3).jpg";
 function Home() {
   let title = "first";
   if (title === "first")
     return (
       <div className="container my-5">
-        <img src={img} alt="img" />
-        <article>this is article</article>
+        <div className="row">
+          <div className="col-md-4">
+            <img src={img1} alt="img1" className="w-100" />
+          </div>
+          <div className="col-md-4">
+            <img src={img2} alt="img1" className="w-100" />
+          </div>
+          <div className="col-md-4">
+            <img src={img3} alt="img1" className="w-100" />
+          </div>
+        </div>
+        <article className="text-center">
+          <p className="my-2 fw-bold">this is article</p>
+        </article>
       </div>
     );
   else if (title === "second")
